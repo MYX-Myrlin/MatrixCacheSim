2ee23c358b39cd9c304675a495cdb69ce1174427
Zach Wilson <myrlin@dragonflight.io>
2018-11-27 14:37:54 -0800

    Fix bug with Bootstrap + Duktape.

--- config/boot.rb
+++ config/boot.rb
@@ -2,3 +2,7 @@ ENV['BUNDLE_GEMFILE'] ||= File.expand_path('../Gemfile', __dir__)
 
 require 'bundler/setup' # Set up gems listed in the Gemfile.
 require 'bootsnap/setup' # Speed up boot time by caching expensive operations.
+
+# Set the ExecJS runtime to use NodeJS to avoid an "Undefined not callable" bug
+# that occurs when using Bootstrap & Duktape
+ENV['EXECJS_RUNTIME'] = 'Node'
