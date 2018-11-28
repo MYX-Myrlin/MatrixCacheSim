265d1ae7f782664b009b7648a4cf5c1bb4baf435
Zach Wilson <myrlin@dragonflight.io>
2018-11-27 16:03:41 -0800

    Add controller for the main menu.

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,9 @@
 Rails.application.routes.draw do
+  # Use the menu page as the root page
+  root 'menu#index'
+  # Available pages
+  get 'menu/index'
   get 'simulator/ThirtyTwo'
   get 'simulator/SixtyFour'
   get 'simulator/SixtyThree'
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
